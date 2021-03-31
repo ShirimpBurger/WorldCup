df87f59a8dc6aa91c71a40a40e6a4407d335c838
hakzzang <mbh930312@naver.com>
2021-04-01 00:20:05 +0900

    [feat] BaseActivity 추가

---
+++ app/src/main/java/com/hbs/worldcup/core/BaseActivity.kt
@@ -0,0 +1,24 @@
+package com.hbs.worldcup.core
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.hbs.domain.model.core.ActivityInitializer
+
+abstract class BaseActivity<B : ViewDataBinding> : AppCompatActivity() {
+    lateinit var binding: B
+    abstract fun getActivityInitializer(): ActivityInitializer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindActivity()
+    }
+
+    private fun bindActivity() = getActivityInitializer()
+        .takeIf { it.isUseTransition }
+        ?.let {
+            binding = DataBindingUtil.setContentView(this, it.layoutId)
+            binding.lifecycleOwner = this
+        }
+}
