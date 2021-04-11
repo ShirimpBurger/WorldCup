98c76a3feb4f763de281255f09fcf3ae8405d8ab
hakzzang <mbh930312@naver.com>
2021-04-11 23:38:36 +0900

    [feat] LargeCardREcommend 리스트 및 바인딩 추가

---
+++ app/src/main/java/com/hbs/worldcup/ui/quizlist/LargeCardRecommendAdapter.kt
@@ -0,0 +1,41 @@
+package com.hbs.worldcup.ui.quizlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hbs.worldcup.databinding.NewQuizItemBinding
+
+data class LargeCardRecommend(val title:String, val thumbnail:String, val rating:String, val chipTitle:String)
+
+class LargeCardRecommendAdapter (private val callback: Callback) : ListAdapter<LargeCardRecommend, LargeCardRecommendAdapter.ViewHolder>(diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = NewQuizItemBinding.inflate(LayoutInflater.from(parent.context), parent , false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.binding) {
+            item = getItem(position)
+            root.setOnClickListener { callback.click(getItem(position), position) }
+            executePendingBindings()
+        }
+    }
+
+    class ViewHolder(val binding: NewQuizItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    fun interface Callback{
+        fun click(largeCardRecommend: LargeCardRecommend, position: Int)
+    }
+}
+
+private val diffUtil = object : DiffUtil.ItemCallback<LargeCardRecommend>() {
+    override fun areItemsTheSame(oldItem: LargeCardRecommend, newItem: LargeCardRecommend): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: LargeCardRecommend, newItem: LargeCardRecommend): Boolean {
+        return oldItem == newItem
+    }
+}
