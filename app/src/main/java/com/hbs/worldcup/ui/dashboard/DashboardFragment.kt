a02953bf04f411388370a667e5d406e6329d6b70
문병학(Byeonghak Moon)/App개발팀/11ST <hakzzang@sk.com>
2021-05-16 23:44:16 +0900

    [feat] 각종 Fragment 기본 규격 추가

---
+++ app/src/main/java/com/hbs/worldcup/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,21 @@
+package com.hbs.worldcup.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hbs.worldcup.databinding.DashboardFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DashboardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DashboardFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
