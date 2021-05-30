e8d2d36074f93e61e351d2014545c12851556f51
hakzzang <mbh930312@naver.com>
2021-05-30 20:36:13 +0900

    [ui] Parallax 뷰 개선

--- app/src/main/java/com/hbs/worldcup/ui/quiz/QuizViewPagerAdapter.kt
+++ app/src/main/java/com/hbs/worldcup/ui/quiz/QuizViewPagerAdapter.kt
@@ -1,34 +1,55 @@
 package com.hbs.worldcup.ui.quiz
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.hbs.domain.model.QuizItem
 import com.hbs.worldcup.databinding.QuizItemBinding
-import com.hbs.worldcup.ui.custom.ParallaxScrollingView
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 
-class QuizViewPagerAdapter(private val completeQuizListener: CompleteQuizListener, private val progressListener: ProgressListener) :
+class QuizViewPagerAdapter(
+    private val completeQuizListener: CompleteQuizListener,
+    private val progressListener: ProgressListener
+) :
     ListAdapter<QuizItem, QuizViewPagerAdapter.ViewHolder>(diffUtil) {
     class ViewHolder(val binding: QuizItemBinding) : RecyclerView.ViewHolder(binding.root)
 
+    private val deviceWidth by lazy {
+        Resources.getSystem().displayMetrics.widthPixels
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = QuizItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding.selectionAImageView.layoutParams = binding.selectionAImageView.layoutParams.apply {
+            width = deviceWidth
+        }
+        binding.selectionBImageView.layoutParams = binding.selectionBImageView.layoutParams.apply {
+            width = deviceWidth
+        }
         return ViewHolder(binding)
     }
 
     @FlowPreview
     @ExperimentalCoroutinesApi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val parallaxScrollingView = holder.binding.parallaxScrollingView
-        parallaxScrollingView.destinationListener = ParallaxScrollingView.DestinationListener {
-            completeQuizListener.complete(position)
+        val parallaxScrollingView = holder.binding.scrollView
+        parallaxScrollingView.post {
+            parallaxScrollingView.scrollTo(deviceWidth / 2, 0)
         }
-        parallaxScrollingView.progressListener = ParallaxScrollingView.ProgressListener {
-            progressListener.progress(it)
+
+
+        parallaxScrollingView.setOnScrollChangeListener { _, scrollX, _, _, _ ->
+            if (deviceWidth / 2 > scrollX) {
+                holder.binding.selectionAImageView.translationX = (scrollX / 2).toFloat()
+            } else {
+                holder.binding.selectionAImageView.translationX = (deviceWidth / 2) - (scrollX / 2).toFloat()
+            }
         }
     }
 
