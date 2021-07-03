49b48a761794656f0dfdd943c3d385ed8b0d6ef3
hakzzang <mbh930312@naver.com>
2021-07-03 11:37:13 +0900

    [data] GameEntity를 파싱하는 로직 수정

--- data/src/main/java/com/hbs/data/remote/mappers/JsonToEntity.kt
+++ data/src/main/java/com/hbs/data/remote/mappers/JsonToEntity.kt
@@ -1,10 +1,29 @@
 package com.hbs.data.remote.mappers
 
-import com.google.gson.Gson
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
-import com.hbs.data.remote.model.GameEntity
+import com.hbs.data.local.model.GameEntities
+import com.hbs.data.local.model.GameEntity
+import java.lang.reflect.Type
 
-fun String.toGameEntity() : List<GameEntity> {
+fun String.toGameEntities(filed: String): List<GameEntity> {
     val gameEntityType = object : TypeToken<List<GameEntity>>() {}.type
-    return Gson().fromJson(this, gameEntityType)
+
+    return GsonBuilder().registerTypeAdapter(gameEntityType, GameEntityDeserializer(filed))
+        .create()
+        .fromJson(this, gameEntityType)
+}
+
+class GameEntityDeserializer(private val filed: String) : JsonDeserializer<GameEntities> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): GameEntities {
+        return json?.asJsonArray?.map {
+            val title = it.asJsonObject["name"].asString
+            val thumbnail = it.asJsonObject["thumbnail"].asString
+            GameEntity(title, thumbnail, filed)
+        }?.toList() ?: emptyList()
+    }
 }
